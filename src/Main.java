e387febd36331826fa134e369876f2c18f908bd1
Neto test <loktionov.dv@mail.ru>
2022-12-20 18:40:07 +0300

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-        int deposit = 1_100;  // Сколько денег кладем на счет
+        int moneyInAccount = 100; // Денег было на счете
+        System.out.println("Предыдущий остаток на счете: " + moneyInAccount + " руб.");
+        int deposit = 1_100;      // Сколько денег кладем на счет
         System.out.println("Вы внесли: " + deposit + " руб.");
 
-        int cond = 1_000;
-        boolean c = deposit >= cond;
+        int conditionFor = 1_000;
+        boolean condition = deposit >= conditionFor;
 
         int sum;
-        if (c) {
-            sum = deposit + deposit/100;
-        }
-        else {
-            sum = deposit;
+        if (condition) {
+            sum = moneyInAccount + deposit + deposit / 100;
+        } else {
+            sum = moneyInAccount + deposit;
         }
 
-        System.out.println("Ваш счет пополнен на: " + sum);
+        System.out.println("Ваш счет пополнен на: " + sum + " руб.");
     }
 }
